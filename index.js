3b59ec7b1a2afdbc562f1a0cdf5d6a65a4c3d8cc
Victoria Mei <vm930@hotmail.com>
2019-03-29 22:56:47 -0400

    done

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+let newID;
+newID = oldID + 1000000000;
+
+let ageIsValid = Number.isInteger(currentAge);
+
+let randomNumber = Math.floor(Math.random() * 21 )+1;
+
+// randomInteger, which should take the value in randomNumber and round it down to the nearest integer.
+
+let randomInteger = Math.floor(randomNumber);
+
+//randomUserID, which should convert randomInteger into a valid ID number — an integer between 1000000001 and 1000000020.
+
+let randomUserID = randomInteger + 1000000000;
